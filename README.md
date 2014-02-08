52a4a04283fbcdee0265e9a4a9d014642bb6594b
Krzysztof Lojniewski <kontakt@pagepro.pl>
2014-02-08 17:58:22 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-blockclicker
-============
+#blockClicker
+
+
+###Benefits
+* You can allow (mobile) users to click on whole block element.
+* Bigger clickable area.
+* No need to change HTML.
+
+
+##Installation
+
+###Step 1: Link plugin file
+
+```html
+<script src="/js/jquery.blockclicker.js"></script>
+```
+
+###Step 2: Call the blockClicker
+
+Call .blockClicker() on block element. Note that the call must be made inside of a $(document).ready() call, or the plugin will not work!
+
+```javascript
+$(document).ready(function(){
+  $('.feed-econ').blockClicker({
+    link: 'h4 a'
+  });
+});
+```
+
+##Configuration options
+
+
+**link**
+Selector of link inside block element
+```
+default: 'h4 a'
+options: jQuery selector
+```
---
+++ jquery.blockclicker.js
@@ -0,0 +1,37 @@
+/*globals jQuery, window, document */
+(function ($) {
+    "use strict";
+    var pluginName  =   "blockClicker",
+        defaults    =   {
+            link:     'h4 a'
+        };
+    // The actual plugin constructor
+    function Plugin(element, options) {
+        this.element        =   element;
+        this.$element       =   $(this.element);
+        this.settings       =   $.extend({}, defaults, options);
+        this.$firstInput    =   $(this.settings.firstFieldSelector);
+        this.$secondInput   =   $(this.settings.secondFieldSelector);
+        this.init();
+    }
+    Plugin.prototype = {
+        init: function () {
+            var that = this;
+            this.$element.on('click', function () {
+                $(this).find(that.settings.link).trigger('click');
+            });
+            this.$element.on('click', that.settings.link, function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                window.location = this.href;
+            });
+        }
+    };
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+}(jQuery));
